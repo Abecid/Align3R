79aeeddfad75263e34619215225e7ba7f0346e01
Abecid <cmlee3211@gmail.com>
2025-02-15 07:11:17 +0000

    Use MonST3r checkpoints as base

--- train.sh
+++ train.sh
@@ -8,7 +8,7 @@ CUDA_VISIBLE_DEVICES="2,3" torchrun --master_port=2569 --nproc_per_node 2 tool/t
                           --train_criterion="ConfLoss(Regr3D(L21, norm_mode='avg_dis'), alpha=0.2)" \
                           --test_criterion="Regr3D_ScaleShiftInv(L21, gt_scale=True)" \
                           --model="AsymmetricCroCo3DStereo(pos_embed='RoPE100', patch_embed_cls='ManyAR_PatchEmbed', img_size=(512, 512), head_type='dpt', output_mode='pts3d', depth_mode=('exp', -inf, inf), conf_mode=('exp', 1, inf), enc_embed_dim=1024, enc_depth=24, enc_num_heads=16, dec_embed_dim=768, dec_depth=12, dec_num_heads=12)" \
-                          --pretrained="ckpt/DUSt3R_ViTLarge_BaseDecoder_512_dpt.pth" \
+                          --pretrained="ckpt/MonST3R_PO-TA-S-W_ViTLarge_BaseDecoder_512_dpt.pth" \
                           --lr=0.00005 --min_lr=1e-06 --warmup_epochs=5 --epochs=50 --batch_size=2 --accum_iter=4 \
                           --save_freq=1 --keep_freq=5 --eval_freq=1 --print_freq=10 --disable_cudnn_benchmark \
                           --output_dir="checkpoints/Align3R_512dpt_finedynamic"
